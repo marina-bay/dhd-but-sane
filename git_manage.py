3a8f2b68e798ff0201ef36f0d440051434740e32
ix5 <ix5@users.noreply.github.com>
2019-03-09 22:12:16 +0100

    Initial commit

---
+++ git_manage.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python2
+
+# See https://pymotw.com/2/multiprocessing/communication.html
+import multiprocessing
+import os
+import subprocess
+
+_current_dir = os.getcwd()
+
+
+class Consumer(multiprocessing.Process):
+
+    def __init__(self, task_queue, result_queue):
+        multiprocessing.Process.__init__(self)
+        self.task_queue = task_queue
+        self.result_queue = result_queue
+
+    def run(self):
+        while True:
+            next_task = self.task_queue.get()
+            if next_task is None:
+                # Poison pill means shutdown
+                self.task_queue.task_done()
+                break
+            answer = next_task()
+            self.task_queue.task_done()
+            self.result_queue.put(answer)
+        return
+
+
+class ManageRepoTask(object):
+    def __init__(self, package):
+        self.url = package['url']
+        self.folder_name = package['folder_name']
+
+    def __call__(self):
+        manage_git_repo(self.url, self.folder_name)
+
+    def __str__(self):
+        return 'url: %s' % (self.url)
+
+
+def manage_git_repo(url, folder_name):
+    _repo_abspath = os.path.join(_current_dir, folder_name)
+    if os.path.isdir(_repo_abspath):
+        # Only pull updates
+        _operation = 'updating'
+        print("Updating %s" % folder_name)
+        p = subprocess.Popen(
+                # -C sets working dir for git
+                ['git', '-C', _repo_abspath, 'pull']
+        )
+    else:
+        # Fresh clone
+        _operation = 'cloning'
+        print("Cloning %s" % url)
+        p = subprocess.Popen(
+              ['git', 'clone', url, folder_name],
+              stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+              #encoding='utf8'  #python3 only
+        )
+    res = p.communicate()
+    if res[1]:
+        print("Error while %s: %s" % (_operation, res[1]))
+    return p.returncode
---
+++ main.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python2
+
+# Clone in parallel
+# https://stackoverflow.com/questions/26023395/how-to-speed-up-parallelize-downloads-of-git-submodules-using-git-clone-recu/34762036#34762036
+# See https://pymotw.com/2/multiprocessing/communication.html
+# Update repos if necessary
+# May use fixed revisions but discouraged
+
+import multiprocessing
+import yaml
+
+import git_manage
+
+DEFAULT_TARGETS_FILE = 'default-mw-targets.yaml'
+
+with open(DEFAULT_TARGETS_FILE, 'r') as f:
+    middleware_targets = yaml.safe_load(f)
+
+_packages = []
+
+for item in middleware_targets['common_middlewares']:
+    _packages.append(
+            {'url': item['url'],
+             'folder_name': "hybris/%s" % item['url'].split('/')[-1]}
+    )
+
+if __name__ == '__main__':
+    # Establish communication queues
+    tasks = multiprocessing.JoinableQueue()
+    results = multiprocessing.Queue()
+
+    # Start consumers
+    #num_consumers = multiprocessing.cpu_count() * 2
+    # Better throttle a bit since Github's not happy with many clones at once
+    num_consumers = 4
+    consumers = [git_manage.Consumer(tasks, results)
+                 for i in xrange(num_consumers)]
+    for w in consumers:
+        w.start()
+
+    # Enqueue jobs
+    num_jobs = len(_packages)
+    for i in xrange(num_jobs):
+        tasks.put(git_manage.ManageRepoTask(_packages[i]))
+
+    # Add a poison pill for each consumer
+    for i in xrange(num_consumers):
+        tasks.put(None)
+
+    # Wait for all of the tasks to finish
+    tasks.join()
+
+    # Start printing results
+    while num_jobs:
+        result = results.get()
+        num_jobs -= 1
